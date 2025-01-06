378d5468bcd2b98e5f5106da0e0fa5a59ccf76ca
goldibb <igornejman13@gmail.com>
2025-01-06 12:45:26 +0100

    Add SuperheroViewModel to manage superhero data and superpowers

---
+++ Webapp/Models/Superheroes/SuperheroViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Webapp.Models.Superheroes;
+
+public class SuperheroViewModel
+{
+    public Superhero Superhero { get; set; }
+    public List<Superpower> AvailableSuperPowers { get; set; }
+    public List<int> SelectedSuperPowers { get; set; }
+}
